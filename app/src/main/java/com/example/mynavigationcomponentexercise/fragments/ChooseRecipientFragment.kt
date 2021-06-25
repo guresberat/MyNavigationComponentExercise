54cd087c95b53bce7548fa7ab598549f2389d4f8
Beratcan Güreş <guresberatcan@gmail.com>
2021-06-25 11:05:19 +0300

    Fourth Commit

--- app/src/main/java/com/example/mynavigationcomponentexercise/fragments/ChooseRecipientFragment.kt
+++ app/src/main/java/com/example/mynavigationcomponentexercise/fragments/ChooseRecipientFragment.kt
@@ -1,13 +1,18 @@
 package com.example.mynavigationcomponentexercise.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.mynavigationcomponentexercise.R
 
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +20,21 @@ class ChooseRecipientFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.next_btn -> {
+                navController.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            }
+            R.id.cancel_btn -> requireActivity().onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/mynavigationcomponentexercise/fragments/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/mynavigationcomponentexercise/fragments/SpecifyAmountFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.mynavigationcomponentexercise.R
 
-class SpecifyAmountFragment : Fragment() {
+class SpecifyAmountFragment : Fragment(), View.OnClickListener  {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +20,21 @@ class SpecifyAmountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.send_btn -> {
+                navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            }
+            R.id.cancel_btn -> requireActivity().onBackPressed()
+        }
     }
 }
